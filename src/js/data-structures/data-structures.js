8d2481ab9928b246d3f8d82fa2a86222f9f3b35c
Ankit Patel <ankitpansuriya007@gmail.com>
2021-07-01 19:26:16 +0530

    updated folder structure

--- src/js/data-structures/data-structures.js
+++ src/js/data-structures/data-structures.js
@@ -1,21 +1,5 @@
-/*
-* Data Structures - allow you to manage data
-* Arrays: [1,2,3] | Ordered List of data | duplicate allowed
-* Objects: {name: 'Ankit'} | Key:value pair of unordered data
-* Maps: new Map();map.set('a','b') | key: value pairs of ordered, iterable data
-* Sets: new Set(); set.add(1) | Unordered List | No Duplicate allowed
-*/
+import './default-data-structures/primitive/primitive';
+import './default-data-structures/non-primitive/non-primitive';
 
-/**
- * WeakSet | WeakMap: Variatons of Set and Map
- * Values and keys are only 'Weakly Referenced'
- * Garbage collection can delete keys and values if not used anywhere in the app
- */
-
-import './arrays';
-import './sets';
-import './objects';
-import './maps';
-
-import './custom-data-structures';
+import './custom-data-structures/custom-data-structures';
 
